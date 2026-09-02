92db6465ebf3cacd49163fc4d4e6a024f51714a5
DeSebasWeb <juanselopezhernandez@gmail.com>
2026-09-01 22:01:00 -0500

    Initializate proyect with backend fixed version

--- server/src/index.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { createServer } from './infrastructure/http/server';
-
-const port = process.env.PORT ?? 3000;
-const app = createServer();
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
